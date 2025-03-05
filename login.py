189fc3e787b40b108266ae1f2c78b7ba5dc91f0e
adhikaribibek231 <159928438+adhikaribibek231@users.noreply.github.com>
2025-03-05 10:37:05 +0545

    Update login.py

--- login.py
+++ login.py
@@ -5,7 +5,7 @@ from utils import login_user, set_login_status
 
 st.set_page_config(page_title="Login")
 
-st.sidebar.button("Home", on_click=lambda: st.switch_page("home.py"))
+st.sidebar.button("Home", on_click=lambda: st.switch_page("streamlit_app.py"))
 st.sidebar.button("Login", on_click=lambda: st.switch_page("login.py"))
 st.sidebar.button("Register", on_click=lambda: st.switch_page("register.py"))
 
