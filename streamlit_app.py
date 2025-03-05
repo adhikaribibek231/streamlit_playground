c0f4b27051dd1208876f423e9b929e7830004aac
adhikaribibek231 <159928438+adhikaribibek231@users.noreply.github.com>
2025-03-05 10:34:08 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+from utils import check_login_status
 
-st.title('🎈 App Name')
+st.set_page_config(page_title="Home")
 
-st.write('Hello world!')
+# Navigation buttons
+st.sidebar.button("Home", on_click=lambda: st.switch_page("home.py"))
+st.sidebar.button("Login", on_click=lambda: st.switch_page("login.py"))
+st.sidebar.button("Register", on_click=lambda: st.switch_page("register.py"))
+
+st.title("Welcome to Home Page")
+
+if check_login_status():
+    st.write("Hello, World!")
+    st.write("Hi, how are you?")
+else:
+    st.write("Hello, World!")
+    st.info("Please log in to see more content.")
